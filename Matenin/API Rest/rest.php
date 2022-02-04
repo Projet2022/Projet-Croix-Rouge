eec24f233ae0955e98e0b4dfb6f8db088db609b5
Projet2022 <98593716+Projet2022@users.noreply.github.com>
2022-02-04 15:41:54 +0100

    .

--- Matenin/API Rest/rest.php
+++ Matenin/API Rest/rest.php
@@ -1,5 +1,7 @@
 <?php
 
+/////////////////////////// GET //////////////////////////////////
+
 // Connexion Base de données
 require ('./model.php');
 $id = ConnectDB();
@@ -9,24 +11,33 @@ $req_type = $_SERVER["REQUEST_METHOD"];
 $req_path = $_SERVER["PATH_INFO"];
 $req_data = explode("/", $req_path);
 
-// Affichage Quantite minimale d'article atteinte
-if($req_type === "GET"){
-	echo "GET";
-	if(isset($req_data[1]) && $req_data[1] === "") 
-		{
-			$requeteSQL = "SELECT nomCategorie,quantite_min FROM TypeProduit";
-        }
-    echo $requeteSQL;
-}
+// Test RequeteSQL
 
-// Affichage Produit a date de peremption depassee
-if($req_type === "GET"){
-	echo "GET";
-	if(isset($req_data[1]) && $req_data[1] === "") 
-		{
-			$requeteSQL = "SELECT datePeremption FROM Produit ORDER BY datePeremption";
-        }
-    echo $requeteSQL;
-}
+$requeteSQL = "SELECT NomTypeProduit,Quantitémin FROM TypeProduit";
+echo $requeteSQL;
 
-?>
+
+// // Affichage Quantite minimale d'article atteinte
+// if($req_type === "GET"){
+// 	echo "GET";
+// 	if(isset($req_data[1]) && $req_data[1] === "") 
+// 		{
+// 			$requeteSQL = "SELECT NomTypeProduit,Quantitémin FROM TypeProduit";
+//         }
+//     echo $requeteSQL;
+// }
+
+// // Affichage Produit a date de peremption depassee
+// if($req_type === "GET"){
+// 	echo "GET";
+// 	if(isset($req_data[1]) && $req_data[1] === "") 
+// 		{
+// 			$requeteSQL = "SELECT datePeremption FROM Produit ORDER BY datePeremption";
+//         }
+//     echo $requeteSQL;
+// }
+
+// ?>
+
+
+<!-- INSERT INTO `TypeProduit` (`idTypeProduit`, `NomTypeProduit`, `idCategorie`, `Emplacement`, `Quantitémin`, `QrCode`) VALUES ('', '', '', '', '', '') -->
